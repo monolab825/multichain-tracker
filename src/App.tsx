65eed30da1d49dc08cd46bca5bb1510e78ea766a
monolab825 <p.smiles777@gmail.com>
2024-05-13 05:00:41 -0300

    add dependenciesp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>MultiChain Assets Tracker</h1>
+    <div className="m-2">
+      <h1 className="text-3xl font-bold">
+        MultiChain Assets Tracker
+      </h1>
+    </div>  
     </>
   )
 }
