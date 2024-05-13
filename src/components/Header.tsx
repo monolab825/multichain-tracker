fe703395562e383507f3648736aa6f02af3e9168
monolab825 <p.smiles777@gmail.com>
2024-05-13 08:20:11 -0300

    header bg  tide up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png"
 const Header = () => {
   return (
     <header>
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl dark:text-white">
         MultiChain Assets Tracker
       </h1>
       <div className="navbar bg-base-100">
