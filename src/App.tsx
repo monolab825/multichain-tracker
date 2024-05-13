02a2b19525e5f473b769b3d2951f5e03ac4735ef
monolab825 <p.smiles777@gmail.com>
2024-05-13 05:20:15 -0300

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header"
-
+import Footer from "./components/Footer"
 function App() {
   return (
     <>
-    <div className="m-2">
+    <div className="m-3">
       <Header />
+      <Footer />
     </div>  
     </>
   )
