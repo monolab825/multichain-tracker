0efd05fabb802936775d760e69f52fde2fd33231
monolab825 <p.smiles777@gmail.com>
2024-05-13 06:13:51 -0300

    add covalent api and tokens list

--- src/components/TokenList.tsx
+++ src/components/TokenList.tsx
@@ -1,10 +1,37 @@
+import "@covalenthq/goldrush-kit/styles.css";
+import {
+    GoldRushProvider,
+    TokenBalancesListView,
+} from "@covalenthq/goldrush-kit";
+
+
 type ListProps = {
   accountAddress: string;
 }
 
 const TokenList = ({accountAddress}: ListProps) => {
   return (
-    <div>TokenList: {accountAddress}</div>
+    <section>
+      <div>
+        <GoldRushProvider
+          apikey="cqt_rQ7KmpM8xD7gFRBff4Q3kCKKXFff"
+          mode="dark"
+          color="emerald"
+        >
+          <TokenBalancesListView
+            chain_names={[
+              "eth-mainnet",
+              "matic-mainnet",
+              "bsc-mainnet",
+              "avalanche-mainnet",
+              "optimism-mainnet",
+            ]}
+            hide_small_balances
+            address={accountAddress}
+          />
+        </GoldRushProvider>
+      </div>
+    </section>
   )
 }
 
